0852eeec3cc03e23fc1cd0d82483c9e9e72069c9
John Duffie <johnduffie@J3---MBP-M3-Max.lab.sensus.net>
2025-05-28 11:41:56 -0400

    all

---
+++ Project.swift
@@ -0,0 +1,15 @@
+// !$*UTF8*$!
+{
+    archiveVersion = 1;
+    classes = {
+    };
+    objectVersion = 56;
+    objects = {
+        /* Begin PBXBuildFile section */
+        13B07FBC1A68108700A75B9A /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 13B07FB01A68108700A75B9A /* AppDelegate.swift */; };
+        13B07FBF1A68108700A75B9A /* Images.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 13B07FB51A68108700A75B9A /* Images.xcassets */; };
+        13B07FC11A68108700A75B9A /* main.m in Sources */ = {isa = PBXBuildFile; fileRef = 13B07FB71A68108700A75B9A /* main.m */; };
+        /* End PBXBuildFile section */
+    };
+    rootObject = 83CBB9F71A601CBA00E9B192 /* Project object */;
+} 
